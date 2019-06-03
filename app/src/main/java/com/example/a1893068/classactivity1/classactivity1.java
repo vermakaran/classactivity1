fa534e33d6a3f727c65af86eea1f2383fc792042
Karan verma <vermakaran70@ymail.com>
2019-06-03 15:54:08 -0400

    till design

--- app/src/main/java/com/example/a1893068/classactivity1/classactivity1.java
+++ app/src/main/java/com/example/a1893068/classactivity1/classactivity1.java
@@ -1,29 +1,64 @@
 package com.example.a1893068.classactivity1;
+package com.example.a1893068.a1893068classactivity1;
 
+
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class classactivity1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    EditText edt_Fname,edt_Lname,edt_email,edt_pass ;
+    Button btn_sub;
+    String TAG="click submit";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_classactivity1);
+        setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        edt_Fname=findViewById(R.id.FName);
+        edt_Lname=findViewById(R.id.LName);
+        edt_email=findViewById(R.id.eMail);
+        edt_pass=findViewById(R.id.password);
+
+        btn_sub=findViewById(R.id.Submit);
+
+        final Toast mytoast=Toast.makeText(getApplicationContext(),edt_Fname.getText().toString(),Toast.LENGTH_SHORT);
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=edt_Fname.getText().toString();
+                String lname=edt_Lname.getText().toString();
+                String email=edt_email.getText().toString();
+                String password=edt_pass.getText().toString();
+
+
+
+                Log.v(TAG,name);
+                Log.v(TAG,lname);
+                Log.v(TAG,email);
+                Log.v(TAG,edt_pass.getText().toString());
+
+
+                // Toast.makeText(getApplicationContext(),edt_name.getText().toString(),Toast.LENGTH_LONG).show();
+                mytoast.setText("Surya Teja! Thank You");
+                mytoast.setDuration(Toast.LENGTH_LONG);
+
+                mytoast.show();
+                Snackbar.make(v, "Registration is Done", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+
+        });
+
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
